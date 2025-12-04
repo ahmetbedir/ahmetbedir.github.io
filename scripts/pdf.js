8423d72fd02c1fdea67adbe50a9c34dd2a40a938
Ahmet Bedir <ahmet.bedir@despatchcloud.com>
2025-12-04 20:25:31 +0300

    updates

--- scripts/pdf.js
+++ scripts/pdf.js
@@ -1,14 +1,20 @@
-import { promises as fs } from 'fs'
-import * as theme from 'jsonresume-theme-even'
-import puppeteer from 'puppeteer'
-import { render } from 'resumed'
+import { promises as fs } from "fs";
+import * as theme from "jsonresume-theme-even";
+import puppeteer from "puppeteer";
+import { render } from "resumed";
 
-const resume = JSON.parse(await fs.readFile('config/resume.json', 'utf-8'))
-const html = await render(resume, theme)
+const resume = JSON.parse(await fs.readFile("config/resume.json", "utf-8"));
+const html = await render(resume, theme);
 
-const browser = await puppeteer.launch()
-const page = await browser.newPage()
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
 
-await page.setContent(html, { waitUntil: 'networkidle0' })
-await page.pdf({ path: 'dist/resume.pdf', format: 'a4', printBackground: true })
-await browser.close()
+await page.setContent(html, { waitUntil: "networkidle0" });
+await page.pdf({
+  path: "resume/ahmetbedir.pdf",
+  //format: "a1",
+  printBackground: true,
+  height: "2550px",
+  width: "800px",
+});
+await browser.close();
